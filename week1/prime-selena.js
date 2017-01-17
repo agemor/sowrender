b16ed1aad941aacef0d373949031ee79ebd649da
seoHyun99 <seu1140@naver.com>
2017-01-17 14:41:21 +0900

    Add spacing
    I add a space when I write "for" and "if".

--- week1/prime-selena.js
+++ week1/prime-selena.js
@@ -1,11 +1,11 @@
 var inputNumber = 23; // 사용자에게 입력받는 값
 var isPrimeNumber = false;
 
-if(inputNumber == 2){
+if (inputNumber == 2) {
   isPrimeNumber = true;
 } else {
-  for(var tempNum = 2; tempNum < inputNumber; tempNum++){
-    if(inputNumber % tempNum === 0){
+  for (var tempNum = 2; tempNum < inputNumber; tempNum++) {
+    if (inputNumber % tempNum === 0) {
       isPrimeNumber = true;
       break;
     }
