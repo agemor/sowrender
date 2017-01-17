c7d716ab720a70228bb8416e7be1be406c0a78fb
seoHyun99 <seu1140@naver.com>
2017-01-17 23:08:44 +0900

    Add project file about beakhun online judge problem 1446
    This program is incomplete.

---
+++ week1/shortcut-selena/shortcut-selena/shortcut.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shortcut_selena
+{
+    public class ShortCut
+    {
+        public int departurePoint;
+        public int destinationPoint;
+        public int length;
+        public bool isFast = false; // 실제로 빠른지 검사
+
+        public ShortCut(int start, int finish, int len) {
+            this.departurePoint = start;
+            this.destinationPoint = finish;
+            this.length = len;
+            
+            if(len < finish - start) {
+                isFast = true;
+            }
+        }
+    }
+}
