e36776cc3a90d908e4c33281d77f9a502b5c9d06
Erica1217 <ericakim@sowrender.com>
2017-01-20 00:25:05 +0900

    Upload shortcut

--- week1/shortcut/ShortCut/ShortCutTest/Properties/AssemblyInfo.cs
+++ week1/shortcut/ShortCut/ShortCutTest/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 어셈블리에 대한 일반 정보는 다음 특성 집합을 통해 
 // 제어됩니다. 어셈블리와 관련된 정보를 수정하려면
 // 이러한 특성 값을 변경하세요.
-[assembly: AssemblyTitle("csvParser")]
+[assembly: AssemblyTitle("ShortCutTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("csvParser")]
+[assembly: AssemblyProduct("ShortCutTest")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 이 프로젝트가 COM에 노출되는 경우 다음 GUID는 typelib의 ID를 나타냅니다.
-[assembly: Guid("46ff5c55-4bf8-43fe-972f-3e32bf8dffe7")]
+[assembly: Guid("da221355-e6ef-4537-94e7-380214c68a08")]
 
 // 어셈블리의 버전 정보는 다음 네 가지 값으로 구성됩니다.
 //
