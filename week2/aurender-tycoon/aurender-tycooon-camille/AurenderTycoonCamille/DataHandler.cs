9fb7ee68d2b3fadce2ab89b7b1910b21d5447f3f
Camille-Jeong <camillejeong@sowrender.com>
2017-01-26 00:05:05 +0900

    Design functional structure

---
+++ week2/aurender-tycoon/aurender-tycooon-camille/AurenderTycoonCamille/DataHandler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AurenderTycoonCamille
+{
+    /**
+     * This class gathers et cetera details
+     * 
+     * date 2017/01/25
+     */
+    class DataHandler
+    {
+        /* string processing */
+        public void OrderKeyword(string purchaseOrder)
+        {
+            //saperate purchase order
+        }
+
+        /* translate options to number */
+        public int SelectedOption()
+        {
+            int optionNum = 0;
+            //each options to number
+            return optionNum;
+        }
+
+        /* compare new client data with list of client */
+        public bool CompareClient(Dictionary<string, ClientData> allClient)
+        {
+            bool isExiste = false;
+            //if there are same client, send 1
+            //else send 0 
+
+            return isExiste;
+        }
+    }
+}
--- week2/aurender-tycoon/aurender-tycooon-camille/AurenderTycoonCamille/Program.cs
+++ week2/aurender-tycoon/aurender-tycooon-camille/AurenderTycoonCamille/Program.cs
@@ -10,7 +10,22 @@ namespace AurenderTycoonCamille
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello world");
+            string purchaseOrder;
+            /*이름, 구매하고 싶은 기기, 수량, 배송지, 연락처*/
+            Console.WriteLine("이름/구매할 기기/수량/배송지/연락처를 입력하세요");
+            purchaseOrder = Console.ReadLine();
         }
+    }
+
+    /**
+     * This class defines product info
+     * 
+     * @date 2017.01.25
+     */
+    class ProductData
+    {
+        //detail
+        //amount
+        //explain
     }
 }
