1ae6f294580ddb9d9863681a59b5fa38010ec2b1
seoHyun99 <seu1140@naver.com>
2017-01-26 15:22:45 +0900

    Update class file

--- week2/aurender-tycoon/aurender-tycoon-selena/AurenderTycoonSelena/ClientManager.cs
+++ week2/aurender-tycoon/aurender-tycoon-selena/AurenderTycoonSelena/ClientManager.cs
@@ -8,7 +8,10 @@ namespace AurenderTycoonSelena
 {
     class ClientManager
     {
+
         // dbmanager 불러와서 고객 정보 저장
+
+
         private DBManager dbManager = new DBManager();
         //dbManager.createDb(parameter);
 
@@ -19,6 +22,9 @@ namespace AurenderTycoonSelena
 
         public void getClientInfo(string clientName)
         {
+            //db에 접근해서 clientName을 key로 데이터 가져오기
+
+            //todo: 중복 처리
         }
     }
 }
--- week2/aurender-tycoon/aurender-tycoon-selena/AurenderTycoonSelena/ReceiptManager.cs
+++ week2/aurender-tycoon/aurender-tycoon-selena/AurenderTycoonSelena/ReceiptManager.cs
@@ -8,7 +8,11 @@ namespace AurenderTycoonSelena
 {
     class ReceiptManager
     {
-        public void addReceiptFromDb() { }
+        ReceiptData[] receipt;
+
+        public void addReceiptFromDb( ) {
+            //모든 구매가 끝났을 경우, db로 전송
+        }
 
         public void deleteReceiptFromDb() { }
     }
