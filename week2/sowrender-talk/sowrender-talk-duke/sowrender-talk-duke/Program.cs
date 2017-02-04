e28b1cfbbbe7b25a0f5fb008ef3258a1bbe5512a
Duke-Yang <dukeyang@sowrender.com>
2017-02-04 14:35:49 +0900

    Upload sowrendertalk

---
+++ week2/sowrender-talk/sowrender-talk-duke/sowrender-talk-duke/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace sowrender_talk_duke
+{
+    class Program
+    {
+        private static NetworkStream stream;
+        private static object Lock = new object();
+        static void Main(string[] args)
+        {          
+            TcpClient socket = new TcpClient();
+            //IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("12.0.0.1"), 13333);
+
+            Console.WriteLine("SorenderTalk v1.0");
+
+            try
+            {
+                socket.Connect("192.168.0.12", 10001);
+                //socket.Connect(endPoint);
+            }
+            catch (Exception e)
+            {
+                //Console.WriteLine(e.ToString());
+            }
+            if (socket.Connected)
+            {
+                Console.WriteLine("success");
+            }
+            else
+            {
+                Console.WriteLine("fail");
+                return;
+            }
+
+            stream = socket.GetStream();
+            //StreamReader reader = new StreamReader(stream);
+
+            Thread thread = new Thread(watchServer);
+            thread.Start();
+            //thread.Join();
+            //thread.Interrupt();
+
+            while (true)
+            {
+                string messege = Console.ReadLine();
+                byte[] enmes = Encoding.UTF8.GetBytes(messege);
+                //string original = Encoding.UTF8.GetString(encoded);
+                stream.Write(enmes, 0, enmes.Length);
+                stream.Flush();
+            }         
+        }
+
+        private static void watchServer()
+        {
+            while (true)
+            {
+                byte[] buffer = new byte[2048];
+
+                lock (Lock)
+                {
+                    int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                    string data = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                    Console.WriteLine(data);
+                }
+            }
+        }
+    }
+}
---
+++ week2/sowrender-talk/sowrender-talk-duke/sowrender-talk-duke/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("sowrender-talk-duke")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("sowrender-talk-duke")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9ca241e9-7c1a-4b96-8d98-48d7900a1fcd")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
