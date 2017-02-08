e0075f56d44cb1dac7cddc3ca105af1c10e9bdf0
Camille-Jeong <camillejeong@sowrender.com>
2017-02-08 10:37:32 +0900

    New yew-or-no project
    and upload image source  in splash page

---
+++ week3/yes-or-no/yes-or-no-camille/yes-or-no-camille/yes_or_no_camille.Droid/MainActivity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace yes_or_no_camille.Droid
+{
+    [Activity(Label = "yes_or_no_camille", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App());
+        }
+    }
+}
---
+++ week3/yes-or-no/yes-or-no-camille/yes-or-no-camille/yes_or_no_camille/SplashPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace yes_or_no_camille
+{
+    public partial class SplashPage : ContentPage
+    {
+        public SplashPage()
+        {
+            InitializeComponent();
+
+            Image wow = new Image();
+            wow.Source = ImageSource.FromFile("wow.jpg");
+        }
+    }
+}
