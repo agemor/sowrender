0e2f14a4b9d765959545e0235a3a5b00c8e10726
seoHyun99 <seu1140@naver.com>
2017-02-09 17:13:32 +0900

    Upload yes-or-no project developed by xamarin

---
+++ week3/yes-or-no-selena/YesOrNo/YesOrNo-/YesOrNo_/EntryPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace YesOrNo_
+{
+    public partial class EntryPage : ContentPage
+    {
+        public EntryPage()
+        {
+            InitializeComponent();
+        }
+
+        async void EntryClicked(object sender, EventArgs args)
+        {
+            await Navigation.PushAsync(new ResponsePage());
+        }
+    }
+}
---
+++ week3/yes-or-no-selena/YesOrNo/YesOrNo-/YesOrNo_/ResponsePage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace YesOrNo_
+{
+    public partial class ResponsePage : ContentPage
+    {
+        public ResponsePage()
+        {
+            InitializeComponent();
+        }
+
+        async void ExitClicked(object sender, EventArgs args)
+        {
+            await Navigation.PopAsync();
+        }
+        async void SettingClicked(object sender, EventArgs args)
+        {
+            await Navigation.PushAsync(new SettingPage());
+        }
+
+        void NoClicked(object sender, EventArgs args)
+        {
+            //YesButton.SetValue("BackgroundColor", "#ffffff");
+            //((Button) YesButton).
+            //NoButton.BackgroundColor = Color.Red;
+        }
+        void YesClicked(object sender, EventArgs args)
+        {
+            //NoButton.BackgroundColor = Color.White;
+            //YesButton.BackgroundColor = Color.Green;
+        }
+    }
+}
